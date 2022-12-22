720bd564e594366df48b60b0e096a5dbbf1ea532
Surajit Mirbar <surajitmirbar980@gmail.com>
2022-12-22 15:31:47 +0530

    adding ui

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-/*
-Project: Rock Paper Scissors
-*/
+let userScore=0;
+let round=5
+let computerScore=0;
+
+
 function getComputerChoice(){
     let choice=Math.floor(Math.random()*3);
     if(choice === 0){
@@ -15,86 +17,38 @@ function getComputerChoice(){
     
 }
 
-function getUserChoice(){
-    let choice=prompt("Enter your Choice","rock / paper / scissor");
-    return choice;
-}
+function startGame(userChoice) {
+    let computerChoice=getComputerChoice();
 
-function startGame() {
-    // let userChoice=getUserChoice();
-    // let computerChoice=getComputerChoice();
-
-    // if (userChoice === computerChoice) {
-    //     console.log("its tie "+userChoice+" "+computerChoice);
-    //     startGame();
-    // }
-
-    // if(userChoice === "rock"){
-    //     if (computerChoice === "scissor") {
-    //         console.log("You won "+userChoice+" beats "+computerChoice);
-    //     }else{
-    //         console.log("You lose "+computerChoice+" beats "+userChoice);
-    //     }
-    // }else if (userChoice === "paper") {
-    //     if (computerChoice === "rock") {
-    //         console.log("You won "+userChoice+" beats "+computerChoice);
-    //     }else{
-    //         console.log("You lose "+computerChoice+" beats "+userChoice);
-    //     }
-    // }else if (userChoice === "scissor") {
-    //     if (computerChoice === "paper") {
-    //         console.log("You won "+userChoice+" beats "+computerChoice);
-    //     }else{
-    //         console.log("You lose "+computerChoice+" beats "+userChoice);
-    //     }
-    // }else{
-    //     console.log("wrong choice please enter as write in input")
-    //     startGame();
-    // }
-    let userScore=0;
-    let computerScore=0;
-    let round=1;
-    while (round<=5) {
-        let userChoice=getUserChoice();
-        let computerChoice=getComputerChoice();
-        
-        if (userChoice === computerChoice) {
-            console.log("its a tie you both choose "+computerChoice);
-            continue;
-        }else if (userChoice === "rock") {
-            if (computerChoice === "scissor") {
-                userScore++;
-                console.log("You won round "+round+" "+userChoice+" beats "+computerChoice);
-            }else{
-                computerScore++;
-                console.log("You lose round "+round+" "+computerChoice+" beats "+userChoice);
-            }
-        }else if (userChoice === "paper") {
-            if (computerChoice === "rock") {
-                userScore++;
-                console.log("You won round "+round+" "+userChoice+" beats "+computerChoice);
-            }else{
-                computerScore++;
-                console.log("You lose round "+round+" "+computerChoice+" beats "+userChoice);
-            }
-        }else if (userChoice === "scissor") {
-            if (computerChoice === "paper") {
-                userScore++;
-                console.log("You won round "+round+" "+userChoice+" beats "+computerChoice);
-            }else{
-                computerScore++;
-                console.log("You lose round "+round+" "+computerChoice+" beats "+userChoice);
-            }
+    if(userChoice === "rock"){
+        if (computerChoice === "scissor") {
+            userScore++;
+            document.getElementById("userscore-p").innerHTML=userScore;
+            document.getElementById("result-text").innerHTML="You won Rock beats Scissor";
         }else{
-            console.log("wrong choice please enter as write in input")
-            continue;
+            computerScore++;
+            document.getElementById("computerscore-p").innerHTML=computerScore;
+            document.getElementById("result-text").innerHTML="You lose Rock cannot beats Paper";
         }
-        round++;
-    }
-    if (userScore>computerScore) {
-        console.log("userwon");
-    } else {
-        console.log("computerwon");
+    }else if (userChoice === "paper") {
+        if (computerChoice === "rock") {
+            userScore++;
+            document.getElementById("userscore-p").innerHTML=userScore;
+            document.getElementById("result-text").innerHTML="You won Paper beats Rock";
+        }else{
+            computerScore++;
+            document.getElementById("computerscore-p").innerHTML=computerScore;
+            document.getElementById("result-text").innerHTML="You lose Paper cannot beats Scissor";
+        }
+    }else if (userChoice === "scissor") {
+        if (computerChoice === "paper") {
+            userScore++;
+            document.getElementById("userscore-p").innerHTML=userScore;
+            document.getElementById("result-text").innerHTML="You won Scissor beats Paper";
+        }else{
+            computerScore++;
+            document.getElementById("computerscore-p").innerHTML=computerScore;
+            document.getElementById("result-text").innerHTML="You lose Scissor cannot beats Rock";
+        }
     }
 }
-startGame();
